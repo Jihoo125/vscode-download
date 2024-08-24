d763ebc3619a9b6d239ccdd895f5a2d80a5694ef
Jihoo125 <jihoo1408010@gmail.com>
2024-08-24 13:20:40 +0900

    Automated commit

--- samsung.py
+++ samsung.py
@@ -1,1 +1,1 @@
-print("이번 역은 삼성(무역센터) 역입니다. 내리실 문은 왼쪽입니다."); print("This stop is Samseong (World Trade Center Seoul) Station, Samsung Station. The doors are on your left.");
+print("이번 역은 삼성(무역센터) 역입니다. 내리실 문은 왼쪽입니다."); print("This station is Samseong (World Trade Center Seoul) Station, Samsung Station. The doors are on your left.");
