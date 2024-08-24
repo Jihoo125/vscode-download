69e2367a106435ba154c03eae5274257c46a2ef8
Jihoo125 <jihoo1408010@gmail.com>
2024-08-24 12:55:10 +0900

    Automated commit

--- b.py
+++ b.py
@@ -1,3 +1,3 @@
 import qrcode
 qrcode.make(2/2
-(data: QRData | bytes | str, *, version: ConvertibleToInt | None = None, error_correction: Literal[0, 1, 2, 3] = 0, box_size: ConvertibleToInt = 10, border: ConvertibleToInt = 4, image_factory: type[GenericImage@make], mask_pattern: _MaskPattern | None = None) -> GenericImage@make)
+(data: QRData | bytes | str, *, version: ConvertibleToInt | None = None, error_correction: Literal[0, 1, 2, 3] = 0, box_size: ConvertibleToInt = 10, border: ConvertibleToInt = 4, image_factory: type[GenericImage@make], mask_pattern: _MaskPattern | None = None) -> GenericImage@makea)
