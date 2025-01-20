f4f4a6983326789ac1b967cf1c38329343a705fd
jihoo125 <jihoo1408010@gmail.com>
2025-01-20 18:24:59 +0900

    Automated commit

--- ch20/js/ggd1.js
+++ ch20/js/ggd1.js
@@ -2,7 +2,7 @@ var i, x, y, z;
 var y = prompt("몇단까지")
 var z = prompt("몇단까지")
 		
-for (i = 1; i <= 9; i++) {
+for (z = 1; i <= 9; i++) {
     document.write("<h3>" + z + "단</h3>");
     for (x = 1; x <= y; x++) {
         document.write(z +" X " + x + " = " + i*x + "<br>");
