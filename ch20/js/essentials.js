a1c2287e876d873ffdb501f06a9fd87747d3765f
jihoo125 <jihoo1408010@gmail.com>
2025-01-20 17:49:17 +0900

    Automated commit

--- ch20/js/essentials.js
+++ ch20/js/essentials.js
@@ -1,3 +1,3 @@
 console.log("j1tet ch20 essentials loaded");
 console.log("https://github.com/Jihoo125/vscode-download/blob/main/ch20/js/essentials.js");
-console.log("(c)j1tet 2024 ~ 2025")
+console.log("(c)j1tet 2024 ~ 2025");
