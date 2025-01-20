337a9f66b85fbdcd459d1fa812291aec8359ba2b
jihoo125 <jihoo1408010@gmail.com>
2025-01-20 17:51:14 +0900

    Automated commit

--- ch20/js/switch.js
+++ ch20/js/switch.js
@@ -2,4 +2,6 @@ var session = prompt("선택해 임마(1, 2, 3)");
 
 switch(session){
     case "1" : document.write("<p>뭐</p>")
+    case "2" : document.write("<p>뭐</p>")
+    case "3" : document.write("<p>뭐</p>")
 }
