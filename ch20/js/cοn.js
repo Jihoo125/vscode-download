12d924c72dd921230d5cfa743ad668dede9bc270
jihoo125 <jihoo1408010@gmail.com>
2025-01-20 18:12:28 +0900

    Automated commit

--- ch20/js/cοn.js
+++ ch20/js/cοn.js
@@ -3,8 +3,9 @@ var n = 10;
 var sum = 0;
 
 for (i = 1; i <= n; i++){
-    if (i % 2 === 1){}
+    if (i % 2 === 1){
         continue
+        
     sum += i;
 
 document.write(i + " ------ " + sum + "<br>");
