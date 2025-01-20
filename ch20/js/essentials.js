7993b18f17bb032b9d4a79eb7bf0993692ccd3eb
jihoo125 <jihoo1408010@gmail.com>
2025-01-20 17:55:03 +0900

    Automated commit

--- ch20/js/essentials.js
+++ ch20/js/essentials.js
@@ -1,3 +1,10 @@
 console.log("j1tet ch20 essentials loaded");
 console.log("https://github.com/Jihoo125/vscode-download/blob/main/ch20/js/essentials.js");
 console.log("(c)j1tet 2024 ~ 2025");
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(() => {
+        console.log('Copied to clipboard');
+    }).catch(err => {
+        console.error('Failed to copy', err);
+    });
+}
